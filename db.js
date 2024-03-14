1d5f3b2955c509ceae41994993ad78c0a8b84728
MajesticGi <163383352+MajesticGi@users.noreply.github.com>
2024-03-14 03:12:31 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,17 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host:"localhost",
-    port:5432,
-    password:"8896",
-    database:"gsite",
-    user:"playabook"
+   
+   connectionString:"postgres://gsite_frxm_user:T4e8MbTsbY7Gk5VdJ2FYFFnlfL3fKuvi@dpg-cnp9nhv79t8c73b76870-a.oregon-postgres.render.com/gsite_frxm",
+    
+   ssl:{
+    rejectUnauthorized:false
+   }
+   // host:"localhost",
+    // port:5432,
+    // password:"8896",
+    // database:"gsite",
+    // user:"playabook"
 });
 
 
